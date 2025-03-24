6239656c30ce200973c00e249cc66283d020ed0e
robbianobastien <bastien.robbiano@live.fr>
2025-03-24 17:01:38 +0100

    home page

---
+++ vite-project/src/Component/Feature.jsx
@@ -0,0 +1,20 @@
+import './Feature.scss'
+
+function Feature({img,title,description}) {
+    return(
+    <div className="feature-item">
+          <img
+            src={img}
+            alt="Chat Icon"
+            className="feature-icon"
+          />
+          <h3 className="feature-item-title">{title}</h3>
+          <p>
+            {description}
+          </p>
+        </div>
+        )
+}
+
+
+export default Feature  
--- vite-project/src/Page/Home.jsx
+++ vite-project/src/Page/Home.jsx
@@ -1,42 +1,27 @@
 import './Home.scss'
 import Hero from '../Component/Hero'
+import Feature from '../Component/Feature'
+import iconMoney from '../assets/image/icon-money.png'
+import iconChat from '../assets/image/icon-chat.png'
+import iconSecurity from '../assets/image/icon-security.png'
 
 function home() {
     return(<div>
         <Hero />
       <section className="features">
         <h2 className="sr-only">Features</h2>
-        <div className="feature-item">
-          <img src="./img/icon-chat.png" alt="Chat Icon" className="feature-icon" />
-          <h3 className="feature-item-title">You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src="./img/icon-money.png"
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">More savings means higher rates</h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src="./img/icon-security.png"
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
-        </div>
+        <Feature img={iconChat} 
+        title="you are our #1 priority" 
+        description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." 
+        />
+        <Feature img={iconMoney} 
+        title="More savings means higher rates" 
+        description="The more you save with us, the higher your interest rate will be!" 
+        />
+       <Feature img={iconSecurity} 
+        title="Security you can trust" 
+        description="we use top of the line encryption to make sure your data and money is always safe." 
+        />
       </section>
     </div>
 
